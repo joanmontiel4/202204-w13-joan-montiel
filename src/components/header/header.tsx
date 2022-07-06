8e7f5debbc96ad23f4ca472564f6cf18290eb540
joanmontiel4 <joanmontiel@gmail.com>
2022-07-06 10:17:28 +0200

    Add header.tsx

---
+++ src/components/header/header.tsx
@@ -0,0 +1,25 @@
+export function Header({
+    appTitle,
+    children,
+}: {
+    appTitle: string;
+    children: JSX.Element;
+}) {
+    const logo = './logo.svg';
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    {appTitle}
+                </a>
+            </h1>
+            {children}
+        </header>
+    );
+}

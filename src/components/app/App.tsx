15ffbf8570d494d26220d8cf557322f1acb062b7
joanmontiel4 <joanmontiel@gmail.com>
2022-07-06 10:55:59 +0200

    Add app and style

---
+++ src/components/app/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+// import { TodoProvider } from '../../context/todo.provider';
+import { menuOptionsType } from '../../interfaces/menuoptions';
+
+import { Layout } from '../layout/layout';
+
+const Home = React.lazy(() => import('../../pages/home'));
+const Todo = React.lazy(() => import('../../pages/todo'));
+const About = React.lazy(() => import('../../pages/about'));
+
+function App() {
+    const appTitle = 'Learning React-Flux';
+    const company = 'ISDI Coders';
+    const menuOptions: menuOptionsType = [
+        { path: 'index', label: 'Home' },
+        { path: 'tasks', label: 'Tasks' },
+        { path: 'about', label: 'About' },
+    ];
+
+    return (
+        // <TodoProvider>
+        <Layout appTitle={appTitle} company={company} menuOptions={menuOptions}>
+            <Routes>
+                <Route
+                    path=""
+                    element={
+                        <React.Suspense>
+                            <Home />
+                        </React.Suspense>
+                    }
+                ></Route>
+                <Route
+                    path="tasks"
+                    element={
+                        <React.Suspense>
+                            <Todo />
+                        </React.Suspense>
+                    }
+                ></Route>
+                <Route
+                    path="about"
+                    element={
+                        <React.Suspense>
+                            <About />
+                        </React.Suspense>
+                    }
+                ></Route>
+                <Route path="*" element={<Navigate replace to="" />}></Route>
+            </Routes>
+        </Layout>
+        // </TodoProvider>
+    );
+}
+
+export default App;
